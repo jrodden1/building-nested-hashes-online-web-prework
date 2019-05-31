146172ea737447f3767dbe6e34c80516ff02677e
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-31 16:01:26 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,7 +15,7 @@ def fourth_challenge
           :age => "18",
           :attitude => "hot-headed"
         }]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -36,6 +36,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
